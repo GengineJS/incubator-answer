ff3cc5f1142349595dad2f588a6e7141f44e2c29
GengineJS <476393671@qq.com>
2024-10-26 15:02:43 +0800

    添加阿里云与redis插件

--- cmd/answer/main.go
+++ cmd/answer/main.go
@@ -21,10 +21,12 @@ package main
 
 import (
 	answercmd "github.com/apache/incubator-answer/cmd"
+	_ "github.com/apache/incubator-answer/external/aliyun_oss"
 	_ "github.com/apache/incubator-answer/external/editor_chart"
 	_ "github.com/apache/incubator-answer/external/editor_formula"
 	_ "github.com/apache/incubator-answer/external/embed_basic"
 	_ "github.com/apache/incubator-answer/external/github"
+	_ "github.com/apache/incubator-answer/external/redis_cache"
 )
 
 // @securityDefinitions.apikey ApiKeyAuth
