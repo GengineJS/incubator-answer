b6b40ff183f56a8208000bac4262a6383d88514d
GengineJS <476393671@qq.com>
2024-10-26 09:26:07 +0800

    插件已经添加

--- ui/src/plugins/index.ts
+++ ui/src/plugins/index.ts
@@ -1,3 +1,5 @@
 export default null;
 
 export { default as editor_chart } from 'editor-chart';
+export { default as editor_formula } from 'editor-formula';
+export { default as embed_basic } from 'embed-basic';
