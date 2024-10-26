ed62f8acf341b83208b8c6756a7f0274461f987d
GengineJS <476393671@qq.com>
2024-10-26 13:41:10 +0800

    aliyunoss与redis cache插件集成

---
+++ external/redis_cache/redis.go
@@ -0,0 +1,194 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package redis
+
+import (
+	"context"
+	_ "embed"
+	"encoding/json"
+	"fmt"
+	"github.com/apache/incubator-answer-plugins/util"
+	"time"
+
+	"github.com/apache/incubator-answer/plugin"
+	"github.com/go-redis/redis/v8"
+
+	"github.com/apache/incubator-answer-plugins/cache-redis/i18n"
+)
+
+var (
+	configuredErr = fmt.Errorf("redis is not configured correctly")
+)
+
+type Cache struct {
+	Config      *CacheConfig
+	RedisClient *redis.Client
+}
+
+type CacheConfig struct {
+	Endpoint string `json:"endpoint"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+func init() {
+	plugin.Register(&Cache{
+		Config: &CacheConfig{},
+	})
+}
+
+func (c *Cache) Info() plugin.Info {
+	info := &util.Info{}
+	info.SlugName = "redis_cache"
+	info.Type = "cache"
+	info.Version = "1.2.8"
+	info.Author = "answerdev"
+	info.Link = "https://github.com/apache/incubator-answer-plugins/tree/main/cache-redis"
+
+	return plugin.Info{
+		Name:        plugin.MakeTranslator(i18n.InfoName),
+		SlugName:    info.SlugName,
+		Description: plugin.MakeTranslator(i18n.InfoDescription),
+		Author:      info.Author,
+		Version:     info.Version,
+		Link:        info.Link,
+	}
+}
+
+func (c *Cache) GetString(ctx context.Context, key string) (data string, exist bool, err error) {
+	if c.RedisClient == nil {
+		return "", false, configuredErr
+	}
+	data, err = c.RedisClient.Get(ctx, key).Result()
+	if err == redis.Nil {
+		return "", false, nil
+	}
+	if err != nil {
+		return "", false, err
+	}
+	return data, true, nil
+}
+
+func (c *Cache) SetString(ctx context.Context, key, value string, ttl time.Duration) error {
+	if c.RedisClient == nil {
+		return configuredErr
+	}
+	return c.RedisClient.Set(ctx, key, value, ttl).Err()
+}
+
+func (c *Cache) GetInt64(ctx context.Context, key string) (data int64, exist bool, err error) {
+	if c.RedisClient == nil {
+		return 0, false, configuredErr
+	}
+	data, err = c.RedisClient.Get(ctx, key).Int64()
+	if err == redis.Nil {
+		return 0, false, nil
+	}
+	if err != nil {
+		return 0, false, err
+	}
+	return data, true, nil
+}
+
+func (c *Cache) SetInt64(ctx context.Context, key string, value int64, ttl time.Duration) error {
+	if c.RedisClient == nil {
+		return configuredErr
+	}
+	return c.RedisClient.Set(ctx, key, value, ttl).Err()
+}
+
+func (c *Cache) Increase(ctx context.Context, key string, value int64) (data int64, err error) {
+	if c.RedisClient == nil {
+		return 0, configuredErr
+	}
+	return c.RedisClient.IncrBy(ctx, key, value).Result()
+}
+
+func (c *Cache) Decrease(ctx context.Context, key string, value int64) (data int64, err error) {
+	if c.RedisClient == nil {
+		return 0, configuredErr
+	}
+	return c.RedisClient.DecrBy(ctx, key, value).Result()
+}
+
+func (c *Cache) Del(ctx context.Context, key string) error {
+	if c.RedisClient == nil {
+		return configuredErr
+	}
+	return c.RedisClient.Del(ctx, key).Err()
+}
+
+func (c *Cache) Flush(ctx context.Context) error {
+	if c.RedisClient == nil {
+		return configuredErr
+	}
+	return c.RedisClient.FlushDB(ctx).Err()
+}
+
+func (c *Cache) ConfigFields() []plugin.ConfigField {
+	return []plugin.ConfigField{
+		{
+			Name:        "endpoint",
+			Type:        plugin.ConfigTypeInput,
+			Title:       plugin.MakeTranslator(i18n.ConfigEndpointTitle),
+			Description: plugin.MakeTranslator(i18n.ConfigEndpointDescription),
+			Required:    true,
+			UIOptions: plugin.ConfigFieldUIOptions{
+				InputType: plugin.InputTypeText,
+			},
+			Value: c.Config.Endpoint,
+		},
+		{
+			Name:        "username",
+			Type:        plugin.ConfigTypeInput,
+			Title:       plugin.MakeTranslator(i18n.ConfigUsernameTitle),
+			Description: plugin.MakeTranslator(i18n.ConfigUsernameDescription),
+			Required:    false,
+			UIOptions: plugin.ConfigFieldUIOptions{
+				InputType: plugin.InputTypeText,
+			},
+			Value: c.Config.Username,
+		},
+		{
+			Name:        "password",
+			Type:        plugin.ConfigTypeInput,
+			Title:       plugin.MakeTranslator(i18n.ConfigPasswordTitle),
+			Description: plugin.MakeTranslator(i18n.ConfigPasswordDescription),
+			Required:    false,
+			UIOptions: plugin.ConfigFieldUIOptions{
+				InputType: plugin.InputTypePassword,
+			},
+			Value: c.Config.Password,
+		},
+	}
+}
+
+func (c *Cache) ConfigReceiver(config []byte) error {
+	conf := &CacheConfig{}
+	_ = json.Unmarshal(config, conf)
+	c.Config = conf
+
+	c.RedisClient = redis.NewClient(&redis.Options{
+		Addr:     conf.Endpoint,
+		Username: conf.Username,
+		Password: conf.Password,
+	})
+	return nil
+}
--- go.mod
+++ go.mod
@@ -23,11 +23,14 @@ require (
 	github.com/Chain-Zhang/pinyin v0.1.3
 	github.com/Machiel/slugify v1.0.1
 	github.com/Masterminds/semver/v3 v3.1.1
+	github.com/aliyun/aliyun-oss-go-sdk v3.0.2+incompatible
 	github.com/anargu/gin-brotli v0.0.0-20220116052358-12bf532d5267
+	github.com/apache/incubator-answer-plugins/cache-redis v1.2.8
 	github.com/apache/incubator-answer-plugins/connector-github v1.2.8
 	github.com/apache/incubator-answer-plugins/editor-chart v1.2.9
 	github.com/apache/incubator-answer-plugins/editor-formula v1.2.11
 	github.com/apache/incubator-answer-plugins/embed-basic v1.0.5
+	github.com/apache/incubator-answer-plugins/storage-aliyunoss v1.2.8
 	github.com/apache/incubator-answer-plugins/util v1.0.2
 	github.com/asaskevich/govalidator v0.0.0-20210307081110-f21760c49a8d
 	github.com/bwmarrin/snowflake v0.3.0
@@ -36,6 +39,7 @@ require (
 	github.com/go-playground/locales v0.14.1
 	github.com/go-playground/universal-translator v0.18.1
 	github.com/go-playground/validator/v10 v10.14.0
+	github.com/go-redis/redis/v8 v8.11.5
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/goccy/go-json v0.10.2
 	github.com/golang/mock v1.6.0
@@ -88,10 +92,12 @@ require (
 	github.com/aymerick/douceur v0.2.0 // indirect
 	github.com/bytedance/sonic v1.9.1 // indirect
 	github.com/cenkalti/backoff/v4 v4.2.1 // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
 	github.com/cloudflare/circl v1.1.0 // indirect
 	github.com/containerd/continuity v0.4.2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/docker/cli v24.0.6+incompatible // indirect
 	github.com/docker/docker v24.0.6+incompatible // indirect
 	github.com/docker/go-connections v0.4.0 // indirect
@@ -133,8 +139,6 @@ require (
 	github.com/moby/term v0.5.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
-	github.com/onsi/ginkgo v1.16.5 // indirect
-	github.com/onsi/gomega v1.18.1 // indirect
 	github.com/opencontainers/go-digest v1.0.0 // indirect
 	github.com/opencontainers/image-spec v1.0.2 // indirect
 	github.com/opencontainers/runc v1.1.9 // indirect
@@ -167,6 +171,7 @@ require (
 	golang.org/x/mod v0.12.0 // indirect
 	golang.org/x/sys v0.18.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/time v0.3.0 // indirect
 	golang.org/x/tools v0.13.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.30.0 // indirect
